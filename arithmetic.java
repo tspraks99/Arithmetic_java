7d91b34a2723141f4fdde01fce1af0fd4e925777
Prakash S <79446554+Prakash-develop@users.noreply.github.com>
2021-03-06 20:43:03 -0800

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class arithmetic{
+public static void main(String args[])
+{
+int a=10,b=20;
+System.out.println("a+b="+(a+b));
+System.out.println("a-b="+(a-b));
+System.out.println("a*b="+(a*b));
+System.out.println("a%b="+(a%b));
+System.out.println("a/b="+(a/b));
+}
+}
